049ffc4c6286633937589b1ba693618ff93c5f41
IgnaceVanmechelenPXL <12402669@student.pxl.be>
2024-11-18 13:51:44 +0100

    transferred existing code

---
+++ c-project-mastermind-1/c-project-mastermind-1/MainWindow.xaml.cs
@@ -0,0 +1,140 @@
+﻿using System.Reflection.Emit;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace c_project_mastermind_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int randomIndex;
+        private string randomColor;
+        string[] colors = { "Red", "Yellow", "Orange", "White", "Green", "Blue" };
+        List<string> secretCode = new List<string>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            string randomColorOne = GenerateRandomColor();
+            string randomColorTwo = GenerateRandomColor();
+            string randomColorThree = GenerateRandomColor();
+            string randomColorFour = GenerateRandomColor();
+
+            secretCode.Add(randomColorOne);
+            secretCode.Add(randomColorTwo);
+            secretCode.Add(randomColorThree);
+            secretCode.Add(randomColorFour);
+
+            Title = "MasterMind - " + string.Join(", ", secretCode);
+
+            foreach (string color in colors)
+            {
+                comboBoxOne.Items.Add(color);
+                comboBoxTwo.Items.Add(color);
+                comboBoxThree.Items.Add(color);
+                comboBoxFour.Items.Add(color);
+            }
+        }
+        public string GenerateRandomColor()
+        {
+            Random rnd = new Random();
+            randomIndex = rnd.Next(colors.Length);
+            randomColor = colors[randomIndex];
+            return randomColor;
+        }
+        private void ComboBox_SelectionChanged(object sender, EventArgs e)
+        {
+            ComboBox comboBox = sender as ComboBox;
+
+            if (comboBox != null && comboBox.SelectedItem != null)
+            {
+                string selectedColor = comboBox.SelectedItem.ToString();
+                SolidColorBrush brush = GetBrushFromColorName(selectedColor);
+
+                if (comboBox == comboBoxOne)
+                {
+                    labelOne.Content = selectedColor;
+                    labelOne.Background = brush;
+                }
+                else if (comboBox == comboBoxTwo)
+                {
+                    labelTwo.Content = selectedColor;
+                    labelTwo.Background = brush;
+                }
+                else if (comboBox == comboBoxThree)
+                {
+                    labelThree.Content = selectedColor;
+                    labelThree.Background = brush;
+                }
+                else if (comboBox == comboBoxFour)
+                {
+                    labelFour.Content = selectedColor;
+                    labelFour.Background = brush;
+                }
+            }
+        }
+        private SolidColorBrush GetBrushFromColorName(string colorName)
+        {
+            try
+            {
+                return (SolidColorBrush)new BrushConverter().ConvertFromString(colorName);
+            }
+            catch
+            {
+                return Brushes.Transparent;
+            }
+        }
+        private void CheckCodeButton_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> userColors = new List<string>();
+            userColors.Add(comboBoxOne.SelectedItem?.ToString());
+            userColors.Add(comboBoxTwo.SelectedItem?.ToString());
+            userColors.Add(comboBoxThree.SelectedItem?.ToString());
+            userColors.Add(comboBoxFour.SelectedItem?.ToString());
+
+            for (int i = 0; i < userColors.Count; i++)
+            {
+                if (userColors[i] == secretCode[i])
+                {
+                    SetLabelBorder(i, Colors.DarkRed);
+                }
+                else if (secretCode.Contains(userColors[i]))
+                {
+                    SetLabelBorder(i, Colors.Wheat);
+                }
+            }
+        }
+        private void SetLabelBorder(int index, Color borderColor)
+        {
+            SolidColorBrush brush = new SolidColorBrush(borderColor);
+            switch (index)
+            {
+                case 0:
+                    labelOne.BorderBrush = brush;
+                    labelOne.BorderThickness = new Thickness(3);
+                    break;
+                case 1:
+                    labelTwo.BorderBrush = brush;
+                    labelTwo.BorderThickness = new Thickness(3);
+                    break;
+                case 2:
+                    labelThree.BorderBrush = brush;
+                    labelThree.BorderThickness = new Thickness(3);
+                    break;
+                case 3:
+                    labelFour.BorderBrush = brush;
+                    labelFour.BorderThickness = new Thickness(3);
+                    break;
+            }
+        }
+    }
+}
